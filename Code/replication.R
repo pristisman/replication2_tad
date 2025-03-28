03f2533aeb82af5da03c9c92289a0038b285de13
Priscila Stisman <74566438+pristisman@users.noreply.github.com>
2025-03-28 17:16:39 -0400

    cleaned code and included annotations

--- Code/replication.R
+++ Code/replication.R
@@ -1,8 +1,11 @@
-rm(list=ls()) 
-setwd("/Users/chuchuwan/Desktop/georgetown/2025Spring/PPOL-6801-Data as Text/rep2")#working directory
-options(scipen=999)
-options(max.print=1000000)
+##############################################
+###### REPLICATION CODE FOR TEXT AS DATA ######
+##############################################
+# Author: Chuchu Wen and Priscila Stisman
 
+rm(list=ls()) 
+
+####### Setup
 install.packages("readr")
 install.packages("ggplot2")
 install.packages("ggplotify")
@@ -25,7 +28,8 @@ install.packages("stm")
 library(readr);library(ggplot2);library(stringr);library(plyr);
 library(splitstackshape);library(data.table);library(dplyr);library(cowplot);library(Jmisc);library(psych);library(qwraps2);library(xtable);library(sandwich);library(stargazer);library(lmtest);library(ggpubr);library(multiwayvcov)
 library(wordcloud);library(stm);library(readxl)
-library(ggplotify);library(ggraph);library(tidygraph)
+library(ggplotify);library(ggraph);library(tidygraph); library(here)
+
 require(tm) 
 require(RTextTools) 
 require(qdap) 
@@ -33,17 +37,24 @@ require(qdapDictionaries)
 require(SnowballC) 
 library(tidyverse)
 
-##start 
-data <- read_csv("data_file2.csv")#load data with anonymized processed "text" 
+#setwd("/Users/chuchuwan/Desktop/georgetown/2025Spring/PPOL-6801-Data as Text/rep2") #Chuchu'S working directory
+#setwd(here("Code")) #Priscila's working directory
+options(scipen=999)
+options(max.print=1000000)
+
+## Load Data
+data <- read_csv("data_file2.csv") #load data with anonymized processed "text" 
 data$my_station <- as.factor(data$my_station)
 data <- data.frame(data)
 data.frame(colnames(data))
 
-##
+# Subset corpus based on the complainant gender: male vs. female
 male_complaint <- data[ with(data, grepl("other", data$complainant_gender)),]
 fem_complaint <- data[ with(data, grepl("female", data$complainant_gender)),]
-nongendered <- data[ with(data, grepl(0, data$gendered)),]
-gendered <- data[ with(data, grepl(1, data$gendered)),]
+
+# Subset corpus based on if the report/crime is classified as gendered or non-gendered
+nongendered <- data[ with(data, grepl(0, data$gendered)),] # non-gendered == 0
+gendered <- data[ with(data, grepl(1, data$gendered)),] #gendered == 1
 
 
 ###########
@@ -51,7 +62,8 @@ library(Rtsne)
 library(rsvd)
 library(geometry)
 
-#whole data##########
+###### For the first STM model we work with the whole corpus
+
 set.seed(23456)
 processed <- textProcessor(documents = data$text, metadata = data)
 
@@ -62,15 +74,17 @@ docs <- out$documents
 vocab <- out$vocab
 meta <- out$meta
 
-##builidng model
+## Running Model (1)
 test2 <- stm(documents = out$documents, vocab = out$vocab,
-             K = 32, prevalence =~ complainant_gender + gendered + urban + convicted + acquitted + dismissed,
+             K = 32,
+             prevalence =~ complainant_gender + gendered + urban + convicted + acquitted + dismissed, # Multiple covariates
              max.em.its = 75,
              data = out$meta, init.type = "Spectral",
              verbose = TRUE)
 
-#
+#Create topic vector
 topicnames <- c("(1) UNLICENSED:", "(2) CURRENCY:", "(3) DOWRY-A:", "(4) PHISHING:", "(5) CATTLE:", "(6) RESOURCE MAFIA:","(7) MISSING PERSON:", "(8) DOWRY-B:","(9) RAILWAY:","(10) ACCIDENT/ATTACK:","(11) LEWD BEHAVIOR:","(12) DEVELOPMENT:","(13) INJURY:","(14) DRIVING MISDEMEANOR:", "(15) FUGITIVE:","(16) BURGLARY:","(17) FIGHTING:","(18) ARMS:","(19) ALCOHOL:","(20) PROPERTY:", "(21) DRIVING ACCIDENT:","(22) AUTO THEFT-A:","(23) AUTO THEFT-B:","(24) CHEAT:","(25) MINORITIES:","(26) PHONE THEFT:","(27) KIDNAPPING:","(28) GAMBLING:","(29) CHAIN-SNATCH:","(30) ELECTRICTY THEFT:","(31) DRUGS:","(32) REAL ESTATE:")
+
 
 #**FIGURE1**
 plot(test2, n=6, type = "summary", xlim = c(0, 0.35), main = "All Crime Top Topics", topic.names = topicnames)
@@ -105,8 +119,8 @@ plot(prep2, "gendered", model=test2, method="difference",
 dev.off()
 
 
+###### For the second STM model we work with the corpus subset for only female complainants
 
-#fem_complaint########################################################################################################
 set.seed(23456)
 processed <- textProcessor(documents = fem_complaint$text, metadata = fem_complaint)
 
@@ -117,13 +131,13 @@ docs <- out$documents
 vocab <- out$vocab
 meta <- out$meta
 
-#
+## Running Model (2)
 female_data <- stm(documents = out$documents, vocab = out$vocab,
                    K = 24, prevalence =~ gendered + urban + convicted + acquitted + dismissed,
                    max.em.its = 75,
                    data = out$meta, init.type = "Spectral")
 
-#
+#Create topic vector
 topicnames <- c("(1) VILLAGE PROBLEM:", "(2) POISONING:", "(3) CHEAT:", "(4) UNLICENSED:", "(5) DOWRY-A:", "(6) DOWRY-B:", "(7) REAL ESTATE:", "(8) DEVELOPMENT:", "(9) DOWRY-C:", "(10) CHILD ABUSE/RAPE:", "(11) BURGLARY:", "(12) DOMESTIC VIOLENCE:", "(13) DOWRY-D:", "(14) FIGHTING:", "(15) CHAIN-SNATCH:", "(16) CRIMINAL FORCE:", "(17) RAPE:", "(18) RUNAWAY/SUICIDE:", "(19) MISSING PERSON:", "(20) MISCELLANEOUS:", "(21) PHISHING:", "(22) DRIVING ACCIDENT:", "(23) DOWRY-E:", "(24) ALCOHOL:")
 
 #**FIGURE5**
@@ -132,8 +146,8 @@ plot(female_data, n=6, type = "summary", xlim = c(0, 0.35), main = "Female Compl
 plot(female_data, n=6, type = "summary", xlim = c(0, 0.35), main = "Female Complainant Top FREX", labeltype = "frex",topic.names = topicnames)#What xaxs does is reduce whitespace
 
 
+###### For the third STM model we work with the gendered crime corpus
 
-#gender crime########################################################################################################
 set.seed(23456)
 processed <- textProcessor(documents = gendered$text, metadata = gendered)
 
@@ -144,14 +158,15 @@ docs <- out$documents
 vocab <- out$vocab
 meta <- out$meta
 
-#
+## Running Model (3)
 gendered_data <- stm(documents = out$documents, vocab = out$vocab,
-                     K = 20, prevalence =~ urban + convicted + acquitted + dismissed,
+                     K = 20,
+                     prevalence =~ urban + convicted + acquitted + dismissed, #covariates
                      max.em.its = 75,
                      data = out$meta, init.type = "Spectral")
 
 
-#
+#Create topic vector
 topicnames <- c("(1) DOWRY-MENTAL:", "(2) DOWRY-PHYSICAL:", "(3) DOWRY-PREGNANCY:", "(4) DOWRY-ECONOMIC:", "(5) UNLICENSED (SEX SELECTION):", "(6) DOWRY-RAPE:", "(7) KILLING GIRL CHILD:", "(8) DOWRY DEATH:", "(9) ALCOHOL:", "(10) HURT/DOMESTIC VIOLENCE:", "(11) KIDNAPPING:", "(12) TRAFFICKING:", "(13) BLACKMAIL:", "(14) SEX SELECTION/ABORTION:", "(15) DOWRY-EXTENDED:", "(16) DOWRY-POST COUNSELING:", "(17) RAPE:", "(18) LEWD PHOTOS:", "(19) DOWRY-DESERTION:", "(20) DOWRY-STARVATION:")
 
 #**FIGURE7**
@@ -161,18 +176,20 @@ plot(gendered_data, n=6, type = "summary", xlim = c(0, 0.45), main = "VAW Top FR
 
 
 
-####################################################################################################
-#########################STM-Covariate###########################################
-####################################################################################################
-data$female_complainant <- ifelse(grepl("female", data$complainant_gender), 1, 0)
+####### STM - COVARIATES ####### 
+
+data$female_complainant <- ifelse(grepl("female", data$complainant_gender), 1, 0) #Creating a Gender Covariate
 
 ##
-set.seed(23456)
-processed <- textProcessor(documents = data$text, metadata = data)
+set.seed(23456) #Set paper's seed for reproducibility
+
+processed <- textProcessor(documents = data$text, metadata = data) # Process text documents using the textProcessor() function
+
 
 out <- prepDocuments(documents = processed$documents, 
                      vocab = processed$vocab,
-                     meta = processed$meta, lower.thresh = 500)
+                     meta = processed$meta, lower.thresh = 500) # Remove words that appear fewer than 500 times (lower.thresh = 500)
+
 docs <- out$documents
 vocab <- out$vocab
 meta <- out$meta
@@ -180,19 +197,29 @@ meta <- out$meta
 ##Thanks to Brandon Stewart
 
 ##########
-K <- 32 
-stm.out.c <- stm(out$documents, out$vocab, K=K, prevalence=~
-                   female_complainant, content=~female_complainant,data=out$meta,
-                   max.em.its=25, 
-                   seed=1033311)
+
+K <- 32 #as defined in the paper 
+
+stm.out.c <- stm(
+  out$documents,     # The processed documents
+  out$vocab,         # The vocabulary of the documents
+  K=K,               # Number of topics (32 in this case)
+  prevalence=~female_complainant,   # Covariate affecting topic prevalence
+  content=~female_complainant,      # Covariate affecting topic content 
+  data=out$meta,     # Metadata associated with the documents
+  max.em.its=25,     # Maximum number of EM (Expectation-Maximization) iterations
+  seed=1033311       # Random seed for reproducibility
+)
 
 labelTopics(stm.out.c)
+
 #**FIGURE9**
 plot(stm.out.c, n=6, type = "summary", xlim = c(0, 0.35), main = "All Crime Content Covariate")
 
 
 ## 
 ## 
+
 library(devtools)
 library(stm)
 library(stringr)
@@ -209,9 +236,9 @@ library(stargazer)
 
 
 #######
-#For non-gender related crimes Covariate###################################################################################################
+#For non-gender related crimes Covariate 
 data <- data[with(data, grepl(0, data$gendered)),]
-data$female_complainant <- ifelse(grepl("female", data$complainant_gender), 1, 0)
+data$female_complainant <- ifelse(grepl("female", data$complainant_gender), 1, 0) #female ==1 male == 0
 
 ##
 set.seed(23456)
@@ -219,32 +246,46 @@ processed <- textProcessor(documents = data$text, metadata = data)
 
 out <- prepDocuments(documents = processed$documents, 
                      vocab = processed$vocab,
-                     meta = processed$meta, lower.thresh = 500)
+                     meta = processed$meta, lower.thresh = 500) # Remove words that appear fewer than 500 times (lower.thresh = 500)
+
 docs <- out$documents
 vocab <- out$vocab
 meta <- out$meta
 
 ##########
 K <- 32 
-stm.out.c <- stm(out$documents, out$vocab, K=K, prevalence=~
-                   female_complainant, content=~female_complainant,data=out$meta,
-                 max.em.its=25, 
-                 seed=1033311)
+
+stm.out.c <- stm(
+  out$documents,     # The processed documents
+  out$vocab,         # The vocabulary of the documents
+  K=K,               # Number of topics (32 in this case)
+  prevalence=~female_complainant,   # Covariate affecting topic prevalence 
+  # Allows the presence of a female complainant to affect how frequently different topics appear
+  content=~female_complainant,      # Covariate affecting topic content
+  #Allows the gender of the complainant to influence the words used in each topic
+  data=out$meta,     # Metadata associated with the documents
+  max.em.its=25,     # Maximum number of EM (Expectation-Maximization) iterations
+  seed=1033311       # Random seed for reproducibility
+)
+
 
 labelTopics(stm.out.c)
+
 #**FIGURE10**
 plot(stm.out.c, n=6, type = "summary", xlim = c(0, 0.35), main = "Non-Gender Crime Content Covariate")
 
+################################################ 
+############ EXTENSIONS ########################
+################################################ 
 
-###############################EXTENSION##########################
+#Extension (1)
+##Examining in non-gender related crimes what words are used by men and women, if different or not and how so
 
-##Extension-Examining in non-gender related crimes, what kinds of words are used between different gender complaints
-#E1
-plot(stm.out.c, type = "perspectives", topics =24, main="Word Used for abdution realted crime, by gender of the complaints")
-plot(stm.out.c, type = "perspectives", topics =6, main="Word Used for theft realted crime, by gender of the complaints")
-plot(stm.out.c, type = "perspectives", topics =30, main="Word Used for alcohol realted crime, by gender of the complaints")
+plot(stm.out.c, type = "perspectives", topics =24, main="Word Used for abdution realted crime, by gender of the complaints") #topic 6
+plot(stm.out.c, type = "perspectives", topics =6, main="Word Used for theft realted crime, by gender of the complaints") #topic 24
+plot(stm.out.c, type = "perspectives", topics =30, main="Word Used for alcohol realted crime, by gender of the complaints") #topic 34
 
-############Extension--Same logic but in Gender-related Crime#######
+##Examining in gender related crimes what words are used by men and women, if different or not and how so
 data <- data[with(data, grepl(1, data$gendered)),]
 data$female_complainant <- ifelse(grepl("female", data$complainant_gender), 1, 0)
 
@@ -260,18 +301,20 @@ vocab <- out$vocab
 meta <- out$meta
 
 ##########
-K <- 32
-stm.out.c <- stm(out$documents, out$vocab, K=K, prevalence=~
-                   female_complainant, content=~female_complainant,data=out$meta,
+K <- 32 #number of topics as defined in the paper
+
+stm.out.c <- stm(out$documents, out$vocab, K=K,
+                 prevalence=~female_complainant,
+                 content=~female_complainant,data=out$meta,
                  max.em.its=25, 
                  seed=1033311)
 
 labelTopics(stm.out.c)
-#E2
+
+#Extension (2)
 plot(stm.out.c, n=6, type = "summary", xlim = c(0, 0.35), main = "Gendered Crime Content Covariate")
  
-
-##Extension-Examining in non-gender related crimes, what kinds of words are used between gender
+##Examining in non-gender related crimes what words are used by men and women, if different or now and how so
 compare_content_words <- function(model, topic, group_var = "female_complainant", top_n = 10) {
   logbeta <- model$beta$logbeta
   
@@ -290,13 +333,13 @@ compare_content_words <- function(model, topic, group_var = "female_complainant"
   
   df_top <- df[order(abs(df$diff), decreasing = TRUE), ][1:top_n, ]
   
-  # Reshape for ggplot
+# Reshape for ggplot
   library(tidyr)
   df_long <- df_top |>
     select(word, male, female) |>
     pivot_longer(cols = c("male", "female"), names_to = "group", values_to = "prob")
   
-  # Plot
+# Plot
   library(ggplot2)
   ggplot(df_long, aes(x = reorder(word, prob), y = prob, fill = group)) +
     geom_col(position = "dodge") +
@@ -309,9 +352,10 @@ compare_content_words <- function(model, topic, group_var = "female_complainant"
     theme_minimal(base_size = 14)
 }
 
-#E3
-compare_content_words(stm.out.c, topic = 9, top_n = 12)
-#E4
-compare_content_words(stm.out.c, topic = 18, top_n = 12)
+#Extension (3)
+compare_content_words(stm.out.c, topic = 9, top_n = 12) #Topic 9
+
+#Extension (4)
+compare_content_words(stm.out.c, topic = 18, top_n = 12) #Topic 18
 
 
